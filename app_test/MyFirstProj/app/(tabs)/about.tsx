0204ea4386c5c6243c4768f78ee3f34498a6abea
duasob <zhapebd@gmail.com>
2025-01-18 10:50:42 +0000

    Sync with master

--- app_test/MyFirstProj/app/(tabs)/about.tsx
+++ app_test/MyFirstProj/app/(tabs)/about.tsx
@@ -1,40 +1,21 @@
-import { Platform, View, StyleSheet } from 'react-native';
-import { WebView } from 'react-native-webview';
-
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function AboutScreen() {
-  if (Platform.OS === 'web') {
-    return (
-      <View style={styles.container}>
-        <iframe
-          src="/assets/london_heatmap.html"
-          style={styles.iframe}
-          title="Heatmap"
-        />
-      </View>
-    );
-  }
-
   return (
-    <WebView
-      originWhitelist={['*']}
-      source={{ uri: 'file:///android_asset/london_heatmap.html' }}
-      style={styles.webView}
-    />
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#25292e',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  iframe: {
-    width: '100%',
-    height: '100%',
-  },
-  webView: {
-    flex: 1,
+  text: {
+    color: '#fff',
   },
 });
