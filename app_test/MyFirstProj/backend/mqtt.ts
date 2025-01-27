e5d3d10967b8de878252084b688e0df14d0773ed
Lorenza Giovannetti <lorenza.giovannetti@gmail.com>
2025-01-27 14:04:19 +0000

    Add MQTT test page

---
+++ app_test/MyFirstProj/backend/mqtt.ts
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import mqtt from "mqtt";
+import { Message } from "./message";
+
+export default function MQTT(){
+
+    //not sure if we're gonna do this here??
+
+}
